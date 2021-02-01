4f8d3e4e7c0da4f751e9a273c47da19a0c861d97
Danielle Navarro <d.navarro@unsw.edu.au>
2021-02-01 13:51:17 +1100

    updates banner image

--- static/header/make_banner.R
+++ static/header/make_banner.R
@@ -2,27 +2,40 @@
 library(tidyverse)
 library(e1071)
 
-set.seed(4)
+generate_series <- function(seed) {
 
-n <- 100000
-dat <- tibble(
-  x = 1:n,
-  y = c(rbridge(1, n))
-)
+  set.seed(seed)
 
-for(i in 1:100) {
-  end <- round(rbeta(1, 2, 2) * n)
-  start <- round(rbeta(1, 2, 2) * n)
-  if(start > end) {
-    tmp <- start
-    start <- end
-    end <- tmp
+  n <- 100000
+  dat <- tibble(
+    x = 1:n,
+    y = c(rbridge(1, n)),
+    seed = seed
+  )
+
+  for(i in 1:100) {
+    end <- round(rbeta(1, 2, 2) * n)
+    start <- round(rbeta(1, 2, 2) * n)
+    if(start > end) {
+      tmp <- start
+      start <- end
+      end <- tmp
+    }
+    m <- end - start
+    dat$y[start:end] <- dat$y[start:end] + c(rbridge(1, m + 1))
   }
-  m <- end - start
-  dat$y[start:end] <- dat$y[start:end] + c(rbridge(1, m + 1))
+
+  return(dat)
+
 }
 
-pic <- ggplot(dat, aes(x,y)) +
+dat <- generate_series(4)
+
+long_dat <- map_dfr(100 + (1:5), generate_series)
+
+
+pic <- ggplot(dat, aes(x,y, group = seed)) +
+  geom_path(data = long_dat, colour = "#888888", size = .1) +
   geom_path(colour = "#ff69b4") +
   theme_void() +
   theme(plot.background = element_rect(
